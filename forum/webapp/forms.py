a8dd861b31b0a8d454be0d956649e7171c3ad621
Georg <georgestoneix@gmail.com>
2023-07-29 03:43:18 -0700

    add register

--- forum/webapp/forms.py
+++ forum/webapp/forms.py
@@ -1,18 +1,19 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from django.forms import widgets
-
-from webapp.models import Tag, Article, Comment
+from webapp.models import Item
 
 
 class ItemForm(forms.ModelForm):
-    summary = forms.CharField(max_length=50, label="Название", required=True)
-    create_add = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-
     class Meta:
-        model =
-        fields = ["summary", "author", "start_data", "end_data"]
+        model = Item
+        fields = ["summary", "author", "create_data"]
 
+
+# class CommentForm(forms.ModelForm):
+#     text = forms.Textarea()
+#
+#     class Meta:
+#         model = Comment
+#         fields = ("text",)
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=30, required=False, label="Найти")
