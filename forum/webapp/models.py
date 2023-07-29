121df50e6b36ad4755aa986201ef24d4d781e806
Georg <georgestoneix@gmail.com>
2023-07-29 00:54:30 -0700

    create search

--- forum/webapp/models.py
+++ forum/webapp/models.py
@@ -1,57 +1,17 @@
-from django.contrib.auth import get_user_model
 from django.db import models
-from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-
-class AbstractModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
-
-    class Meta:
-        abstract = True
-
-
-class Article(AbstractModel):
-    title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Название")
-
-    content = models.TextField(max_length=2000, verbose_name="Контент")
-    tags = models.ManyToManyField('webapp.Tag', related_name='articles', blank=True)
+class Item(models.Model):
+    create_data = models.DateField()
+    summary = models.CharField(max_length=50, verbose_name="Заголовок", unique=True, null=False, blank=False)
     author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT,
-                               default=1, related_name="articles", verbose_name="Автор")
-
+                               default=1, related_name="projects", verbose_name="Автор")
     def __str__(self):
-        return f"{self.pk} {self.title}: {self.author}"
-
-    def get_absolute_url(self):
-        return reverse("webapp:article_view", kwargs={"pk": self.pk})  # article/5/
+        return f"{self.id} {self.summary}"
 
     class Meta:
-        db_table = "articles"
-        verbose_name = "Статья"
-        verbose_name_plural = "Статьи"
-        permissions = [
-            ('write_rate', 'Написать рецензию')
-        ]
+        db_table = "Items"
+        verbose_name = "Новость"
+        verbose_name_plural = "Новость"
 
-
-class Comment(AbstractModel):
-    article = models.ForeignKey('webapp.Article', related_name='comments', on_delete=models.CASCADE,
-                                verbose_name='Статья')
-    text = models.TextField(max_length=400, verbose_name='Комментарий')
-    author = models.ForeignKey('auth.User', on_delete=models.SET_DEFAULT,
-                               default=1, related_name="comments", verbose_name="Автор")
-
-    def __str__(self):
-        return self.text[:20]
-
-
-class Tag(AbstractModel):
-    name = models.CharField(max_length=31, verbose_name='Тег')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = "tags"
-        verbose_name = "Тег"
-        verbose_name_plural = "Теги"
